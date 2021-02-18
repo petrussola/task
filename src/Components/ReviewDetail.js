b49bae63b326522ab0df2a7924f2ee35df9227b7
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-02-18 00:42:49 +0000

    Display modal with text of the review

--- src/Components/ReviewDetail.js
+++ src/Components/ReviewDetail.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Styled from "styled-components";
 import ReactStars from "react-rating-stars-component";
+import Modal from "styled-react-modal";
 
 const StyledDiv = Styled.div`
     display: flex;
@@ -9,17 +10,41 @@ const StyledDiv = Styled.div`
     align-items: center;
 `;
 
+const StyledModal = Modal.styled`
+    width: 50%;
+    height: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+`;
+
 export default function ReviewDetail({ review }) {
+  const [isOpen, setIsOpen] = useState(false);
   const reviewNumber = parseInt(review.review_rating[0], 10);
 
+  function toggleModal(e) {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <StyledDiv className="container">
-      {/* <p>{review.review_rating}</p> */}
-      <ReactStars count={5} value={reviewNumber} />
-      <p>{review.profile_name}</p>
-      <p>{review.review_title}</p>
-      <button>Show Review Text</button>
-      <button>Show review details</button>
-    </StyledDiv>
+    <>
+      <StyledDiv className="container">
+        {/* <p>{review.review_rating}</p> */}
+        <ReactStars count={5} value={reviewNumber} />
+        <p>{review.profile_name}</p>
+        <p>{review.review_title}</p>
+        <button onClick={toggleModal}>Show Review Text</button>
+        <button>Show review details</button>
+      </StyledDiv>
+      <StyledModal
+        isOpen={isOpen}
+        onBackgroundClick={toggleModal}
+        onEscapeKeydown={toggleModal}
+      >
+        <span>{review.review_text}</span>
+        <button onClick={toggleModal}>Close me</button>
+      </StyledModal>
+    </>
   );
 }
