27f4daf9c31c8e98212a4e0d0e3e0ac4fd46372b
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-02-18 00:26:59 +0000

    Display stars rating

--- src/Components/ReviewDetail.js
+++ src/Components/ReviewDetail.js
@@ -1,22 +1,21 @@
 import React from "react";
-
 import Styled from "styled-components";
+import ReactStars from "react-rating-stars-component";
 
 const StyledDiv = Styled.div`
     display: flex;
     flex-direction: row;
-    justify-content: flex-start;
+    justify-content: space-around;
     align-items: center;
-    
-    * {
-        width: 15%;
-    }
 `;
 
 export default function ReviewDetail({ review }) {
+  const reviewNumber = parseInt(review.review_rating[0], 10);
+
   return (
-    <StyledDiv>
-      <p>{review.review_rating}</p>
+    <StyledDiv className="container">
+      {/* <p>{review.review_rating}</p> */}
+      <ReactStars count={5} value={reviewNumber} />
       <p>{review.profile_name}</p>
       <p>{review.review_title}</p>
       <button>Show Review Text</button>
