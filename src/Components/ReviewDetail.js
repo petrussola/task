0ca10d044754cf0b5ce1a51fc92ca0efd88f2518
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-02-18 00:00:45 +0000

    Add styled components and display information in columns

--- src/Components/ReviewDetail.js
+++ src/Components/ReviewDetail.js
@@ -1,13 +1,26 @@
 import React from "react";
 
+import Styled from "styled-components";
+
+const StyledDiv = Styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    
+    * {
+        width: 15%;
+    }
+`;
+
 export default function ReviewDetail({ review }) {
   return (
-    <div>
+    <StyledDiv>
       <p>{review.review_rating}</p>
       <p>{review.profile_name}</p>
       <p>{review.review_title}</p>
       <button>Show Review Text</button>
       <button>Show review details</button>
-    </div>
+    </StyledDiv>
   );
 }
--- src/Components/Reviews.js
+++ src/Components/Reviews.js
@@ -1,12 +1,21 @@
 import React from "react";
+import Styled from "styled-components";
+
+// component
 import ReviewDetail from "./ReviewDetail";
+
+const StyledDiv = Styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 1rem;
+`;
 
 export default function Reviews({ reviews }) {
   return (
-    <div>
+    <StyledDiv>
       {reviews.map((review, index) => {
         return <ReviewDetail key={index} review={review} />;
       })}
-    </div>
+    </StyledDiv>
   );
 }
